22fb22b411ab7f6dca7d37c79f429edf6a57051f
amit-punjabi <amitpunjabi17@gmail.com>
2019-03-20 15:14:14 +0530

    with local

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 describe('Google\'s Search Functionality', function() {
   it('can find search results', function () {
     browser
+      .url('http://localhost')
       .url('https://www.google.com/ncr')
       .setValue('*[name="q"]','BrowserStack\n')
       .pause(5000);
